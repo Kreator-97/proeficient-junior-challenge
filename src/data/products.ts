af6d070cef33d86bc97006bc05d851f4b7faa879
Donato Monzon <spartanhalokitty@gmail.com>
2022-11-25 17:49:44 -0600

    feat: Create data to be rendered and their interfaces
    rename svg assets with wrong filenames

---
+++ src/data/products.ts
@@ -0,0 +1,66 @@
+import { Product } from '../interfaces'
+
+import beds         from '../assets/beds.svg'
+import refrigerator from '../assets/refrigerator.svg'
+import forniture    from '../assets/forniture.svg'
+import oven         from '../assets/oven.svg'
+import sofa         from '../assets/sofa.svg'
+import tv           from '../assets/tv.svg'
+import washer       from '../assets/washer-dryer.svg'
+import dining       from '../assets/dining.svg'
+import desk         from '../assets/desk.svg'
+import wardrobe     from '../assets/wardrobe.svg'
+
+export const products: Product[] = [
+  {
+    imgURL: beds,
+    size: 1.2,
+    title: 'Beds'
+  },
+  {
+    imgURL: refrigerator,
+    size: 1.2,
+    title: 'Refrigerator'
+  },
+  {
+    imgURL: forniture,
+    size: 1.2,
+    title: 'forniture'
+  },
+  {
+    imgURL: oven,
+    size: 1.2,
+    title: 'oven'
+  },
+  {
+    imgURL: sofa,
+    size: 1.2,
+    title: 'sofa'
+  },
+  {
+    imgURL: tv,
+    size: 1.2,
+    title: 'tv'
+  },
+  {
+    imgURL: washer,
+    size: 1.2,
+    title: 'washer-dryer'
+  },
+  {
+    imgURL: dining,
+    size: 1.2,
+    title: 'dining'
+  },
+  {
+    imgURL: desk,
+    size: 1.2,
+    title: 'desk'
+  },
+  {
+    imgURL: wardrobe,
+    size: 1.2,
+    title: 'wardrobe'
+  },
+  
+]
---
+++ src/interfaces/product.ts
@@ -0,0 +1,5 @@
+export interface Product {
+  title : string;
+  imgURL: string;
+  size  : number; // m²
+}
